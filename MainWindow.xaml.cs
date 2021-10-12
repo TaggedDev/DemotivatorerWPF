53e7d6f944e3182c48d857060c4a2b6f9dda53c2
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-12 20:28:04 +0300

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Demotivatorer //boba
+namespace Demotivatorer
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
